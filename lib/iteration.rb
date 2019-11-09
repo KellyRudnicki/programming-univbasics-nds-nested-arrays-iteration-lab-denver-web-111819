8f2b34620e25b5151ddaf4a14b3949809ec6c37f
Kelly Rudnicki <kelly.rudnicki@comcast.net>
2019-11-09 05:00:51 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -37,8 +37,8 @@ def total_even_pairs(src)
     
     while inner_array < src.count do
       if src[inner_array][0] && src[inner_array][1] == .even?
-        total <
-        
+        total << src[inner_array][0] +
+      end
         inner_array += 1
       end
     return total
